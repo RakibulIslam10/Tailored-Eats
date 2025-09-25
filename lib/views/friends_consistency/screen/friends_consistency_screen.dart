66020b292c6c7840f00323c541782e6a57a81b78
RakibulIslam10 <rakibulislam10.net@gmail.com>
2025-09-25 15:25:52 +0600

    profile done

--- lib/views/friends_consistency/screen/friends_consistency_screen.dart
+++ lib/views/friends_consistency/screen/friends_consistency_screen.dart
@@ -1,9 +1,7 @@
 import 'package:tailored_eats/views/friends/screen/friends_screen.dart';
-
 import '../../../core/utils/basic_import.dart';
 import '../../../widgets/auth_app_bar.dart';
 import '../controller/friends_consistency_controller.dart';
-
 part 'friends_consistency_screen_mobile.dart';
 
 class FriendsConsistencyScreen extends GetView<FriendsConsistencyController> {
