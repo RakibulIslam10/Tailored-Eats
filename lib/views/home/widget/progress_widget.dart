677bda62993f6f5bec618dab7215f25956b7591d
RakibulIslam10 <rakibulislam10.net@gmail.com>
2025-10-22 13:04:39 +0600

    profile done

--- lib/views/home/widget/progress_widget.dart
+++ lib/views/home/widget/progress_widget.dart
@@ -1,49 +1,5 @@
 part of '../screen/home_screen.dart';
 
-class CalorieProgressPainter extends CustomPainter {
-  final double progress;
-  final double strokeWidth;
-  final Color backgroundColor;
-  final Color progressColor;
-
-  CalorieProgressPainter({
-    required this.progress,
-    this.strokeWidth = 7.0,
-    this.backgroundColor = Colors.cyan,
-    this.progressColor = const Color(0xff4FBAF0),
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final backgroundPaint = Paint()
-      ..strokeWidth = strokeWidth
-      ..color = backgroundColor
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    final progressPaint = Paint()
-      ..strokeWidth = strokeWidth
-      ..color = progressColor
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    final center = Offset(size.width / 2, size.height / 2);
-    final radius = (size.width - strokeWidth) / 2;
-
-    canvas.drawCircle(center, radius, backgroundPaint);
-
-    final rect = Rect.fromCircle(center: center, radius: radius);
-    const startAngle = -3.14 / 2;
-    final sweepAngle = 2 * 3.14 * progress;
-    canvas.drawArc(rect, startAngle, sweepAngle, false, progressPaint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CalorieProgressPainter oldDelegate) {
-    return oldDelegate.progress != progress;
-  }
-}
-
 class CalorieProgressWidget extends GetView<HomeController> {
   final double totalCalories;
   final double size;
@@ -51,7 +7,7 @@ class CalorieProgressWidget extends GetView<HomeController> {
   const CalorieProgressWidget({
     super.key,
     required this.totalCalories,
-    this.size = 75.0,
+    this.size = 76.0,
   });
 
   @override
@@ -71,21 +27,18 @@ class CalorieProgressWidget extends GetView<HomeController> {
               children: [
                 CustomPaint(
                   size: Size(size, size),
-                  painter: CalorieProgressPainter(
-                    strokeWidth: 5.w,
+                  painter: CircularProgressClipper(
                     progress: progress,
-                    progressColor: Color(0xff7AD3FF),
-                    backgroundColor: CustomColors.progressColor.withOpacity(
-                      0.15,
-                    ),
+                    strokeWidth: 7,
                   ),
                 ),
+
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
                       padding: Dimensions.heightSize.edgeBottom * 0.25,
-                      child: Image.asset(Assets.icons.agun.path)
+                      child: Image.asset(Assets.icons.agun.path),
                     ),
                     Wrap(
                       crossAxisAlignment: WrapCrossAlignment.end,
@@ -108,7 +61,6 @@ class CalorieProgressWidget extends GetView<HomeController> {
               ],
             ),
           ),
-          Space.height.v10,
           TextWidget(
             "250/2000",
             fontWeight: FontWeight.bold,
